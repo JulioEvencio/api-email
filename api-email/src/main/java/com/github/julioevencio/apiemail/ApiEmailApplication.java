8b88c503bea1ab072ffed05c0e5c447fb50e68af
JulioEvencio <julioigreja.dev@gmail.com>
2023-09-27 20:21:21 -0300

    added project base

---
+++ api-email/src/main/java/com/github/julioevencio/apiemail/ApiEmailApplication.java
@@ -0,0 +1,13 @@
+package com.github.julioevencio.apiemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmailApplication.class, args);
+	}
+
+}
---
+++ api-email/src/test/java/com/github/julioevencio/apiemail/ApiEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.julioevencio.apiemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
